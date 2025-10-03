897919f9f1cadcf8069ad55280e59e6ad8892784
qhorgues <quentin.horgues@outlook.fr>
2025-10-03 14:29:18 +0200

    java spring initilizer

---
+++ src/main/java/org/group_1/terrain_petanque/TerrainPetanqueApplication.java
@@ -0,0 +1,13 @@
+package org.group_1.terrain_petanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrainPetanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrainPetanqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/group_1/terrain_petanque/TerrainPetanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group_1.terrain_petanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrainPetanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
