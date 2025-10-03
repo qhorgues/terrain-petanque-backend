93bccef9bf6c279e1118c533a0e070feee5becd5
qhorgues <quentin.horgues@outlook.fr>
2025-10-03 16:01:37 +0200

    nix flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Dev shell avec Node.js et npm-check-updates";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        name = "rest_api";
+        version = "0.0.1";
+      in {
+        # packages.default = pkgs.stdenv.mkDerivation {
+        #   pname = name;
+        #   version = version;
+
+        #   src = ./.;
+
+        #   nativeBuildInputs = [
+        #     pkgs.maven
+        #   ];
+        #   buildInputs = [ pkgs.jdk ];
+
+        #   buildPhase = ''
+        #     mvn package
+        #   '';
+
+        #   installPhase = ''
+        #     mkdir -p $out/bin
+        #     cp target/* $out/
+        #     echo "#!/bin/sh" > $out/bin/${name}
+        #     echo "exec ${pkgs.jre}/bin/java -jar $out/${name}-${version}.jar" >> $out/bin/${name}-${version}
+        #     chmod +x $out/bin/${name}-${version}
+        #   '';
+        # };
+        devShells.${system}.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs.buildPackages; [
+            jdk
+            maven
+            jdt-language-server
+          ];
+        };
+      });
+}
