10e203ec2199ab4c2fb857a0e5e9efdd5ca187a0
qhorgues <quentin.horgues@outlook.fr>
2025-10-03 15:53:48 +0200

    config file

---
+++ src/test/java/org/group_1/terrain_petanque/Hello.java
@@ -0,0 +1,13 @@
+package org.group_1.terrain_petanque;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
