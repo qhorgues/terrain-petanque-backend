d16768b4fad968596276d561fdc67387823b7587
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-10-19 00:54:16 +0200

    Ajout de méthode pour sélectionné plusieurs coordonnées.

--- src/main/java/org/group_1/terrain_petanque/repository/CoordinatesRepository.java
+++ src/main/java/org/group_1/terrain_petanque/repository/CoordinatesRepository.java
@@ -1,9 +1,35 @@
-package org.group_1.terrain_petanque.repositories;
+package org.group_1.terrain_petanque.repository;
 
-import org.group_1.terrain_petanque.entity.Coordonnees;
+import java.util.List;
+
+import org.group_1.terrain_petanque.entity.Coordinates;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import io.micrometer.common.lang.NonNull;
+
+/**
+ * This interface represents the repository for coordinates.
+ */
 @Repository
-public interface RepositoryCoordonnees extends JpaRepository<Coordonnees, Long> {
+public interface CoordinatesRepository extends JpaRepository<Coordinates, Integer> {
+
+    /**
+     * This method return a list of coordinates with a specific latitude.
+     * 
+     * @param latitude The latitude.
+     * @return Return a list of coordinates with the specified latitude.
+     */
+    List<Coordinates> findByLatitude(@NonNull String latitude);
+
+
+
+    /**
+     * This method return a list of coordinates with a specific longitude.
+     * 
+     * @param longitude The longitude.
+     * @return Return a list of coordinates with the specified longitude.
+     */
+    List<Coordinates> findByLongitude(@NonNull String longitude);
+
 }
