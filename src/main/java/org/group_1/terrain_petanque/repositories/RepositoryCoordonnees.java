a30dbfe1a57b20e009a21ca55b3cd1b76521013d
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-10-19 16:50:59 +0200

    Repositories and services are not mandatory in this branch. And must be modified (for services).

--- src/main/java/org/group_1/terrain_petanque/repositories/RepositoryCoordonnees.java
+++
@@ -1,9 +0,0 @@
-package org.group_1.terrain_petanque.repositories;
-
-import org.group_1.terrain_petanque.entity.Coordonnees;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryCoordonnees extends JpaRepository<Coordonnees, Long> {
-}
--- src/main/java/org/group_1/terrain_petanque/repositories/RepositoryTerrain.java
+++
@@ -1,8 +0,0 @@
-package org.group_1.terrain_petanque.repositories;
-
-import org.group_1.terrain_petanque.entity.Terrain;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RepositoryTerrain extends JpaRepository<Terrain, Long> {}
