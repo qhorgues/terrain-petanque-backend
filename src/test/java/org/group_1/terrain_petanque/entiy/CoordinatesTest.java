e08981fc3c2c371aee579859654bcc33c3bc18f1
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-10-27 16:23:38 +0100

    Add geLatitude and setLatitude test for the coordinates.

--- src/test/java/org/group_1/terrain_petanque/entiy/CoordinatesTest.java
+++ src/test/java/org/group_1/terrain_petanque/entiy/CoordinatesTest.java
@@ -2,10 +2,7 @@ package org.group_1.terrain_petanque.entiy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Set;
-
 import org.group_1.terrain_petanque.entity.Coordinates;
-import org.group_1.terrain_petanque.entity.Court;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -51,4 +48,44 @@ public class CoordinatesTest {
 
 
 
+    /**
+     * This method tests the getter of the latitude.
+     */
+    @Test
+    public void getLatitudeTest() {
+        // Init & test
+        Coordinates coordinates = new Coordinates("100", "120");
+
+        // Assertion
+        assertEquals(coordinates.getLatitude(), "100");
+    }
+
+
+
+    /**
+     * This method tests the setter of the latitude.
+     */
+    @Test
+    public void setLatitudeTest() {
+        // Init
+        Coordinates coordinates = new Coordinates("100", "120");
+
+        // Test
+        try {
+            coordinates.setLatitude(null);
+            throw new RuntimeException("It cannot be possible to have a latitude with null");
+        }
+        catch (NullPointerException exception) {
+            // The constrain is a success.
+        }
+
+        coordinates.setLatitude("0");
+
+        // Assertion
+        assertEquals(coordinates.getLatitude(), "0");
+    }
+
+
+
+    
 }
