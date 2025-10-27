49a30b1b32e2a816f7d4bb0007c0a0e656a1cf85
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-10-27 16:21:54 +0100

    Add constructor test for the coordinates.

--- src/test/java/org/group_1/terrain_petanque/entiy/CoordinatesTest.java
+++ src/test/java/org/group_1/terrain_petanque/entiy/CoordinatesTest.java
@@ -1,9 +1,34 @@
 package org.group_1.terrain_petanque.entiy;
 
+import org.group_1.terrain_petanque.entity.Coordinates;
+import org.junit.jupiter.api.Test;
+
 /**
  * This class tests the Coordinates entity.
  */
 public class CoordinatesTest {
 
-    
+    /**
+     * This method tests the constructor.
+     */
+    @Test
+    public void constructorTest() {
+        try {
+            new Coordinates("100", null);
+            throw new RuntimeException("It cannot be possible to have a longitude with null");
+        }
+        catch (NullPointerException exception) {
+            // The constrain is a success.
+        }
+
+        try {
+            new Coordinates(null, "120");
+            throw new RuntimeException("It cannot be possible to have a latitude with null");
+        }
+        catch (NullPointerException exception) {
+            // The constrain is a success.
+        }
+    }
+
+
 }
