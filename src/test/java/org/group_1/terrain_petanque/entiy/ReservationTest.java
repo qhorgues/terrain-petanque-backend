362e5c6970cc845da42373e03d4f92176810bea1
Anthony Gimenez <anthony.gimenez@etu.univ-tours.fr>
2025-10-27 16:55:34 +0100

    Add the reservation test.

---
+++ src/test/java/org/group_1/terrain_petanque/entiy/ReservationTest.java
@@ -0,0 +1,107 @@
+package org.group_1.terrain_petanque.entiy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.group_1.terrain_petanque.entity.Coordinates;
+import org.group_1.terrain_petanque.entity.Court;
+import org.group_1.terrain_petanque.entity.Reservation;
+import org.group_1.terrain_petanque.entity.User;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class tests the Court entity.
+ */
+public class ReservationTest {
+    
+    /**
+     * This method tests the constructor.
+     */
+    @Test
+    public void constructorTest() {
+        try {
+            User user = new User("a", "b", "c", "d", "e");
+            new Reservation(user, null, 0);
+            throw new RuntimeException("It cannot be possible to have an user with null");
+        }
+        catch (NullPointerException exception) {
+            // The constrain is a success.
+        }
+
+        try {
+            Court court = new Court("court 1", 1, new Coordinates("100", "120"));
+            new Reservation(null, court, 0);
+            throw new RuntimeException("It cannot be possible to have a court with null");
+        }
+        catch (NullPointerException exception) {
+            // The constrain is a success.
+        }
+    }
+
+
+
+    /**
+     * This method tests the getter the user.
+     */
+    @Test
+    public void getUserTest() {
+        // Init & test
+        User user = new User("a", "b", "c", "d", "e");
+        Court court = new Court("court 1", 1, new Coordinates("100", "120"));
+        Reservation reservation = new Reservation(user, court, 0);
+
+        // Assertion
+        assertEquals(reservation.getUser(), user);
+    }
+
+
+
+    /**
+     * This method tests the getter the court.
+     */
+    @Test
+    public void getCourtTest() {
+        // Init & test
+        User user = new User("a", "b", "c", "d", "e");
+        Court court = new Court("court 1", 1, new Coordinates("100", "120"));
+        Reservation reservation = new Reservation(user, court, 0);
+
+        // Assertion
+        assertEquals(reservation.getCourt(), court);
+    }
+
+
+
+    /**
+     * This method tests the getter the reservation.
+     */
+    @Test
+    public void getReservationTest() {
+        // Init & test
+        User user = new User("a", "b", "c", "d", "e");
+        Court court = new Court("court 1", 1, new Coordinates("100", "120"));
+        Reservation reservation = new Reservation(user, court, 1000);
+
+        // Assertion
+        assertEquals(reservation.getReservation(), 1000);
+    }
+
+
+
+    /**
+     * This method tests the setter the reservation.
+     */
+    @Test
+    public void setReservationTest() {
+        // Init
+        User user = new User("a", "b", "c", "d", "e");
+        Court court = new Court("court 1", 1, new Coordinates("100", "120"));
+        Reservation reservation = new Reservation(user, court, 1000);
+
+        // Test
+        reservation.setReservation(10);
+
+        // Assertion
+        assertEquals(reservation.getReservation(), 10);
+    }
+
+}
